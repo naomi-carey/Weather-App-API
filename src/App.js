9932c3d6c91a778d97d5e32cc52b496111c5d030
Naomi Carey <naomicarey00@gmail.com>
2021-05-24 23:35:32 +0200

    initial creation: weather map layout and api call

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  //setting state equal to an empty object, as I recieve an object
+  const [weather, setWeather] = useState(null);
+  const [input, setInput] = useState("");
+  useEffect(() => {
+    axios
+      .get(
+        `http://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_WEATHER_API}&q=Quito&aqi=no`
+      )
+      .then((data) => {
+        setWeather(data.data);
+        console.log(data.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  //Event listner
+  const weatherInput = (e) => {
+    setInput(e.target.value);
+  };
+
+  const searchWeather = () => {
+    axios
+      .get(
+        `http://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_WEATHER_API}&q=${input}`
+      )
+      .then((data) => {
+        setWeather(data.data);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {weather && (
+        <div>
+          <div className="search">
+            <input onChange={weatherInput} type="text" />
+            <button onClick={searchWeather}>Search</button>
+          </div>
+          <div className="weather-info">
+            <h1>{weather.location.name}</h1>
+            <h2>{weather.location.region}</h2>
+
+            <div className="condition">
+              <h3>{weather.current.condition.text}</h3>
+              <img src={weather.current.condition.icon} alt="weather" />
+              <h3>{weather.current.temp_f} F</h3>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
